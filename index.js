0a47419b09566a65374d0df4245f4abc9415b5af
leandrojunior <>
2021-06-17 21:34:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,114 @@
+var comissao = "cargos"
+console.log("boa tarde a todos!");
+console.log("ao todo somos em 20, quantos estão presente?")
+console.log("do lado esquerdo são 7 pessoas e do direito 8")
+
+function soma (a,b){
+  return a+b
+}
+console.log(soma(7,8,))/console.log("entao ao todo estamos em 15 pessoas");
+
+console.log("bem continuando temos 4 indicaçoes para o cargo de musica, vamos fazer a votação!")
+console.log("================================================")
+ var ana;
+ var diana;
+ var murilo;
+ var pedro;
+
+ana = prompt("quantos votos ana teve?");
+diana = prompt("quantos votos diana teve?");
+murilo = prompt("quantos votos murilo teve?");
+pedro = prompt("quantos votos pedro teve?");
+
+var musica = prompt("bem vamos lá, quem foi o mais votado para este cargo?")
+
+if(musica=="ana"){
+   console.log("ok, agora so depende dele(a) para aceitar o cargo")
+}
+else if (musica=="diana")
+  console.log("ok, agora so depende dele(a) para aceitar o cargo")
+else if (musica=="murilo")
+   console.log("ok, agora so depende dele(a) para aceitar o cargo")
+else if (musica=="pedro")
+   console.log("ok, agora so depende dele(a) para aceitar o cargo")
+
+   else
+   console.log("este nome não esta na lista, mais irei adicionar em breve");
+
+
+   console.log("================================================")
+
+   console.log("Bem vamos prosseguir falando sobre o passeio que teremos!");
+   
+   console.log("Bem a maioria já fez as incrições, assim que eles se cadastrarem, nos vamos enviar um outro formúlario falando bem vindo(a). um exemplo é este aqui")
+  console.log("=================================")
+   sexo = "feminino";
+
+var nome= "Maria"
+switch (sexo){
+  case "feminino":
+  console.log( "Seja bem-vinda " + nome );
+  break;
+  case false:
+  console.log("Seja bem-vindo");
+
+}
+console.log("idade - 18 anos",",quero fazer parte por conta de uma amiga minha-");
+
+console.log("=================================")
+
+console.log("Antes de encerrarmos está reunião, vamos dar uma nota para está comissão!");
+console.log("Vamos lá");
+
+var i =10
+for(var nt=1;nt<2;nt++){
+  console.log("Diego qual sua nota?");
+  console.log(i = "minha nota é 10");
+}
+console.log("=====================")
+var i =10
+for(var nt=1;nt<2;nt++){
+  console.log("Bruno qual sua nota?");
+  console.log(i = "minha nota é 9");
+}
+console.log("=====================")
+
+var i =10
+for(var nt=1;nt<2;nt++){
+  console.log("Vinicius qual sua nota?");
+  console.log(i = "minha nota é 10");
+}
+console.log("=====================")
+
+var i =10
+for(var nt=1;nt<2;nt++){
+  console.log("Fernando qual sua nota?");
+  console.log(i = "minha nota é 10");
+
+}
+
+console.log("=====================")
+
+var inicio = prompt("Obrigados a todos por comparecerem nessa reunião!");
+var num=0
+
+
+while(num<=10){
+  var n= prompt("Obrigados a todos por comparecerem nessa reunião");
+  num++;
+
+}console.log(parseInt(n)+parseInt(n));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
